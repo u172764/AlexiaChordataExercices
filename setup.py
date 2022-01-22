cdbabcfe15a46c1c31597072e1d9f62cf7b8310b
u172764 <72573716+u172764@users.noreply.github.com>
2022-01-22 13:29:38 +0100

    Expose datamembers quaternion

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+ext = Extension("RNG",
+                sources=["RNG.pyx", "mt19937.cpp"],
+                language="c++")
+
+extq = Extension("quaternion",
+                sources=["quaternion.pyx"],
+                language="c++")
+
+
+
+setup(name="RNG",
+      ext_modules=cythonize([ext, extq]))
